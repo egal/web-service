40352772910668f8176af075ad05e178c4fd3848
rollsover <ro.rollsover@gmail.com>
2021-11-12 10:29:26 +0700

    Update: Reformat HTTP response. Upgrade egal/framework (dev-speed_increase 519fe03 => dev-speed_increase 1f39832).

--- app/HttpEgalBridge/Response.php
+++ app/HttpEgalBridge/Response.php
@@ -12,12 +12,20 @@ class Response
 
     public static function toHttpResponse(EgalResponse $response): HttpResponse|HttpResponseFactory
     {
-        $data = [
-            MessageType::ACTION => $response->getActionMessage()->toArray(),
-            MessageType::START_PROCESSING => $response->getStartProcessingMessage()?->toArray(),
-            MessageType::ACTION_RESULT => $response->getActionResultMessage()?->toArray(),
-            MessageType::ACTION_ERROR => $response->getActionErrorMessage()?->toArray(),
-        ];
+        $data = [];
+        $data[MessageType::ACTION] = $response->getActionMessage()->toArray();
+        $actionResult = $response->getActionResultMessage()?->toArray();
+        $actionError = $response->getActionErrorMessage()?->toArray();
+
+        if ($actionResult !== null) {
+            $data[MessageType::ACTION_RESULT] = $actionResult;
+            unset($data[MessageType::ACTION_RESULT][MessageType::ACTION]);
+        }
+
+        if ($actionError !== null) {
+            $data[MessageType::ACTION_ERROR] = $actionResult;
+            unset($data[MessageType::ACTION_ERROR][MessageType::ACTION]);
+        }
 
         return response(json_encode($data), $response->getStatusCode(), ['Content-Type' => 'application/json']);
     }
