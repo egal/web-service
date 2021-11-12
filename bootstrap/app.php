f438b42dd793e1d68b1f4d42af0f015ada3d7370
rollsover <ro.rollsover@gmail.com>
2021-11-12 12:56:15 +0700

    Feature: Config Swoole.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,5 @@
 <?php
 
-use Egal\Core\Bus\Bus;
-use Egal\Core\Bus\BusCreator;
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
@@ -14,10 +11,11 @@ date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 $app = new Egal\Core\Application(dirname(__DIR__));
 
 $app->singleton(Illuminate\Contracts\Debug\ExceptionHandler::class, App\Exceptions\Handler::class);
-$app->singleton(Bus::class, static fn (): Bus => BusCreator::createBus());
+$app->singleton(Egal\Core\Bus\Bus::class, static fn (): Egal\Core\Bus\Bus => Egal\Core\Bus\BusCreator::createBus());
 
 $app->configure('app');
 $app->configure('queue');
+$app->configure('swoole');
 
 $app->register(App\Providers\AppServiceProvider::class);
 
---
+++ config/swoole.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    'http' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | HTTP server configurations.
+        |--------------------------------------------------------------------------
+        |
+        | @see https://www.swoole.co.uk/docs/modules/swoole-server/configuration
+        |
+        */
+        'server' => [
+            'host' => env('SWOOLE_HTTP_HOST', '0.0.0.0'),
+            'port' => (int)env('SWOOLE_HTTP_PORT', 8080),
+            'options' => [
+                'pid_file' => env('SWOOLE_HTTP_PID_FILE', base_path('storage/logs/swoole_http.pid')),
+                'log_file' => env('SWOOLE_HTTP_LOG_FILE', base_path('storage/logs/swoole_http.log')),
+                'reactor_num' => (int)env(
+                    'SWOOLE_HTTP_REACTOR_NUM',
+                    swoole_cpu_num() * (float)env('SWOOLE_HTTP_REACTOR_NUM_MULTIPLIER')
+                ),
+                'worker_num' => (int)env(
+                    'SWOOLE_HTTP_WORKER_NUM',
+                    swoole_cpu_num() * (float)env('SWOOLE_HTTP_WORKER_NUM_MULTIPLIER')
+                ),
+            ],
+        ],
+
+    ]
+
+];
