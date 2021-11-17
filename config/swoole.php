d008c12d217a97b1dd373a1c7bdd55b5887cbee0
rollsover <ro.rollsover@gmail.com>
2021-11-17 13:15:27 +0700

    Update: Refactoring, Upgrading egal/framework (dev-speed_increase a474f1e => dev-speed_increase e27fca0).

--- config/swoole.php
+++ config/swoole.php
@@ -13,18 +13,15 @@ return [
         |
         */
         'server' => [
+
             'host' => env('SWOOLE_HTTP_HOST', '0.0.0.0'),
             'port' => (int)env('SWOOLE_HTTP_PORT', 8080),
+
             'options' => [
-                'reactor_num' => (int)env(
-                    'SWOOLE_HTTP_REACTOR_NUM',
-                    swoole_cpu_num() * (float)env('SWOOLE_HTTP_REACTOR_NUM_MULTIPLIER', 1)
-                ),
-                'worker_num' => (int)env(
-                    'SWOOLE_HTTP_WORKER_NUM',
-                    swoole_cpu_num() * (float)env('SWOOLE_HTTP_WORKER_NUM_MULTIPLIER', 1)
-                ),
+                'reactor_num' => (int)env('SWOOLE_HTTP_REACTOR_NUM', swoole_cpu_num() * (float)env('SWOOLE_HTTP_REACTOR_NUM_MULTIPLIER', 1)),
+                'worker_num' => (int)env('SWOOLE_HTTP_WORKER_NUM', swoole_cpu_num() * (float)env('SWOOLE_HTTP_WORKER_NUM_MULTIPLIER', 1)),
             ],
+
         ],
 
     ]
