071a473cf78aa20c4c6a53273c939930df530308
Аня <Sytuagina@sputnikfund.ru>
2021-12-23 16:58:39 +0700

    Update: set default workers number equal 1.

--- config/swoole.php
+++ config/swoole.php
@@ -18,8 +18,8 @@ return [
             'port' => (int)env('SWOOLE_HTTP_PORT', 8080),
 
             'options' => [
-                'reactor_num' => (int)env('SWOOLE_HTTP_REACTOR_NUM', swoole_cpu_num() * (float)env('SWOOLE_HTTP_REACTOR_NUM_MULTIPLIER', 1)),
-                'worker_num' => (int)env('SWOOLE_HTTP_WORKER_NUM', swoole_cpu_num() * (float)env('SWOOLE_HTTP_WORKER_NUM_MULTIPLIER', 1)),
+                'reactor_num' => (int)env('SWOOLE_HTTP_REACTOR_NUM', 1),
+                'worker_num' => (int)env('SWOOLE_HTTP_WORKER_NUM',1),
             ],
 
         ],
