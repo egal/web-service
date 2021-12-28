5ac395dd1c845c18f22c92479a8e9e43543f827d
Аня <Sytuagina@sputnikfund.ru>
2021-12-28 12:07:27 +0700

    Fix: add setting env params for reactor_num and worker_num if autoconfiguration is off.

--- config/swoole.php
+++ config/swoole.php
@@ -20,10 +20,10 @@ return [
             'options' => [
                 'reactor_num' => env('SWOOLE_AUTO_CONFIG', false)
                     ? (int)env('SWOOLE_HTTP_REACTOR_NUM', swoole_cpu_num() * (float)env('SWOOLE_HTTP_REACTOR_NUM_MULTIPLIER', 1))
-                    : 1,
+                    : (int)env('SWOOLE_HTTP_REACTOR_NUM',1),
                 'worker_num' => env('SWOOLE_AUTO_CONFIG', false)
                     ? (int)env('SWOOLE_HTTP_WORKER_NUM', swoole_cpu_num() * (float)env('SWOOLE_HTTP_WORKER_NUM_MULTIPLIER', 1))
-                    : 1,
+                    : (int)env('SWOOLE_HTTP_WORKER_NUM',1),
             ],
 
         ],
