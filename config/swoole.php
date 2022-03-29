43218459652135196feb17ce8028b4c63a115123
Аня <Sytuagina@sputnikfund.ru>
2022-03-29 18:25:00 +0700

    Update: add package_max_length to swoole config.

--- config/swoole.php
+++ config/swoole.php
@@ -18,6 +18,7 @@ return [
             'port' => (int)env('SWOOLE_HTTP_PORT', 8080),
 
             'options' => [
+                'package_max_length' => env('SWOOLE_PACKAGE_MAX_LENGTH', 5 * 1024 * 1024),
                 'reactor_num' => env('SWOOLE_AUTO_CONFIG', false)
                     ? (int)env('SWOOLE_HTTP_REACTOR_NUM', swoole_cpu_num() * (float)env('SWOOLE_HTTP_REACTOR_NUM_MULTIPLIER', 1))
                     : (int)env('SWOOLE_HTTP_REACTOR_NUM',1),
